7967284ec7497314e5d9e56cf0cb04c27ce16ec8
elmoussaidwalid7-jpg <elmoussaidwalid7@gmail.com>
2025-12-10 16:37:44 +0100

    Add files via upload

---
+++ courses.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+
+
+
+
+var navBar = document.querySelector(".bar")
+window.onscroll = function () {
+    if (window.scrollY >= 50) {
+        navBar.classList.add("background-bar")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
